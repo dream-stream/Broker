522e5e01384503a501465b5ff7bf036a8ebb9bba
Anders Meidahl <Andersmeidahl@gmail.com>
2019-10-17 10:59:15 +0200

    Added delay again this time with 1 sec

--- Dream-Stream/Startup.cs
+++ Dream-Stream/Startup.cs
@@ -63,6 +63,7 @@ namespace Dream_Stream
             var client = env.IsDevelopment() ? new EtcdClient("http://localhost") : new EtcdClient("http://etcd");
             var me = Guid.NewGuid().ToString();
 
+            await Task.Delay(1*1000);
             var brokerTable = new BrokerTable(client);
             await brokerTable.ImHere();
 
